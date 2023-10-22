b108e02288afcebf329b3aedfd49ca68cb7d5241
HusseinnHM <husseinnhm7@gmail.com>
2023-10-23 02:49:02 +0300

    refactor: missing move ExternalProps to file

---
+++ src/ExternalProps.cs
@@ -0,0 +1,19 @@
+using System.Collections.ObjectModel;
+
+namespace Neptunee.OResponse;
+
+public class ExternalProps : ReadOnlyDictionary<string, string>
+{
+    public ExternalProps() : base(new Dictionary<string, string>())
+    {
+    }
+
+    public ExternalProps(IDictionary<string, string> dictionary) : base(dictionary)
+    {
+    }
+
+    internal void TryAdd<TValue>(string key, TValue value)
+    {
+        Dictionary!.TryAdd(key, value!.ToString());
+    }
+}
--- src/OperationResponse.cs
+++ src/OperationResponse.cs
@@ -1,25 +1,8 @@
-﻿using System.Collections.ObjectModel;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Net;
 using Neptunee.OResponse.HttpMessages;
 
 namespace Neptunee.OResponse;
-
-public class ExternalProps : ReadOnlyDictionary<string, string>
-{
-    public ExternalProps() : base(new Dictionary<string, string>())
-    {
-    }
-
-    public ExternalProps(IDictionary<string, string> dictionary) : base(dictionary)
-    {
-    }
-
-    internal void TryAdd<TValue>(string key, TValue value)
-    {
-        Dictionary!.TryAdd(key, value!.ToString());
-    }
-}
 
 public class OperationResponse : OperationResponse<NoResponse>
 {
